a58951589ecaec9c825bf046ea11d0ab19fcfc69
john <Jrowan19>
2019-10-24 09:19:32 +0100

    added styling to error page

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -3,7 +3,7 @@ import { Planet } from 'react-kawaii';
 
 const ErrorPage = ({ error: { status, message } }) => {
   return (
-    <h1 className="text-white" style={{ height: '40rem' }}>
+    <h1 className="text-white bg-dark" style={{ height: '40rem' }}>
       Oh Snap! {status} {message}
       <Planet size={200} mood="blissful" color="#FDA7DC" />;
     </h1>
